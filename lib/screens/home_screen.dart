b514028af25d4f90b39ff94be4ce7894d71b682f
Richard  Danquah <richpee60@yahoo.com>
2023-01-31 11:21:26 +0000

    top header

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -16,7 +16,9 @@ class HomeScreen extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 const HomeScreenNavbar(),
-                const SizedBox(height: 30,),
+                const SizedBox(
+                  height: 30,
+                ),
                 RichText(
                     text: TextSpan(
                         style: Theme.of(context).textTheme.headline1,
@@ -28,7 +30,40 @@ class HomeScreen extends StatelessWidget {
                               .textTheme
                               .headline1!
                               .copyWith(color: kGreyColor900))
-                    ]))
+                    ])),
+                const SizedBox(
+                  height: 24,
+                ),
+                Container(
+                  height: 56,
+                  padding: const EdgeInsets.only(
+                      right: 8, left: 16, bottom: 5, top: 6),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8),
+                    color: kGreyColor500,
+                  ),
+                  child: TextField(
+                    style: Theme.of(context)
+                        .textTheme
+                        .headline5!
+                        .copyWith(color: kBlackColor900),
+                    cursorHeight: 24,
+                    decoration: InputDecoration(
+                        suffixIcon: const Icon(
+                          Icons.search,
+                          color: kBlackColor900,
+                        ),
+                        suffixIconConstraints: const BoxConstraints(
+                          maxHeight: 24,
+                        ),
+                        hintText: 'Search doctor, medical drugs etc',
+                        hintStyle: Theme.of(context).textTheme!.headline5,
+                        contentPadding: const EdgeInsets.only(
+                          bottom: 5,
+                        ),
+                        border: InputBorder.none),
+                  ),
+                )
               ],
             ),
           ),
